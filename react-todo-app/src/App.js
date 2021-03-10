36b25efe1812b158b53c14d196005c83198c14b8
joseph Evangelista <oootep123@gmail.com>
2021-03-09 22:17:33 -0800

    adding event handlers

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,9 +1,18 @@
+import React, {useState} from 'react';
 import './App.css';
+// importing components
+import Form from "../src/components/form";
+import TodoList from "../src/components/todolist"
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Joseph's To do List </h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <TodoList/>
     </div>
   );
 }
